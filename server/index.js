308607eb9a8d8e49580c667ff68e1554f8338514
just4heshan <heshanravindussc@gmail.com>
2023-02-14 20:36:02 +1100

    cors origin setted

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://ai-image-app.netlify.app/"
+}));
 app.use(express.json({limit: "50mb"}));
 
 app.use('/api/v1/post', postRoutes);
